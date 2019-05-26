72394456740d2fbc43d10e55a87b1662729f29cc
0ilia <ilia1mail2martinkevi4@gmail.com>
2019-05-26 23:38:22 +0300

    s

--- Kyrsa4/Menu.xaml.cs
+++ Kyrsa4/Menu.xaml.cs
@@ -96,12 +96,12 @@ namespace Kyrsa4
         
         private void MenuLoad(object sender, RoutedEventArgs e)
         {
-          /*  DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
             dispatcherTimer.Start();
        
-    */}
+    }
         
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
